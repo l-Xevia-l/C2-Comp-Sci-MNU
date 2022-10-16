b5c02d53e8e2d3e7ca7397bf17b0a5b70a10daba
Smaccus <77114635+l-Xevia-l@users.noreply.github.com>
2022-10-16 15:24:08 -0500

    Working on Program1.
    Needs some polishing and debugging, I don't think it should take 1000 attempts to complete a game of 8 up solitaire using only half of the deck.

--- Big Programs/Program1/deckDriver.cpp
+++ Big Programs/Program1/deckDriver.cpp
@@ -15,6 +15,9 @@ Assignment:
 #include <cmath>
 #include <cstdlib>
 #include <String>
+#include <time.h>
+#include <chrono>
+#include <thread>
 
 #include "Deck.h"
 
@@ -26,16 +29,18 @@ void PrintTable(const Card[]);
 
 void PlaceCards(Card[], Deck);
 
-void CoverMatching(Card[], Deck);
+void CoverMatching(Card &currentCard, Card[], Deck, bool &Win, bool &Lost, int &matches);
 
 int main() {
+  srand(time(NULL));
+
   bool Win{false};
+  bool Lost{false};
+
+  int attempts{0};
+  Card currentCard;
+
   Deck deck;
-  deck.GenerateDeck();
-  deck.ResetList();
-  for(unsigned int i{0}; i < 20; i++) {
-    deck.Shuffle();
-  }
 
   // Card slots on table
   Card Card1_1, Card1_2, Card1_3, Card1_4, 
@@ -44,9 +49,36 @@ int main() {
   // Array to simulate a table
   Card cardsOnTable[8] = {Card1_1, Card1_2, Card1_3, Card1_4, 
                           Card2_1, Card2_2, Card2_3, Card2_4};
-  
-  PlaceCards(cardsOnTable, deck);
 
+  while(Win == false){ 
+    int matches{0};
+    
+    // Generate and shuffle the deck
+    deck.GenerateDeck();
+    deck.ResetList();
+    for(unsigned int i{0}; i < (rand() % 50 + 20); i++) {
+      deck.Shuffle();
+    }
+
+
+    deck.ResetList();
+    PlaceCards(cardsOnTable, deck);
+    
+    // Plays one round
+    while(Lost == false && Win == false) {
+      cout << "-------------------------------------------------------";
+      currentCard = deck.GetNextItem();
+      CoverMatching(currentCard, cardsOnTable, deck, Win, Lost, matches);
+    }
+    if(Lost == true){
+      Lost = false;
+      cout << "\n\n\n\n\n\n\n  ------------Attempts: " << attempts << "------------\n\n\n\n\n\n\n";
+      cout << "\n\n  Retrying...\n\n";
+    }
+    //this_thread::sleep_for(std::chrono::nanoseconds(1));    // for debugging
+    attempts++;
+  }
+  cout << "\n\n  Attempts: " << attempts << endl << endl;
   return 0;
 }
 
@@ -72,10 +104,10 @@ void PrintTable(const Card cardsOnTable[8]) {
     string strcard = card.ToString();
 
     if((i < 4) || (i > 4)) {
-      cout << strcard << ' ';
+      cout << strcard << ", ";
     }
     else{
-      cout << '\n' << strcard << ' ';
+      cout << "\n" << strcard << ", ";
     }
   }
 }
@@ -87,21 +119,51 @@ void PlaceCards(Card *cardsOnTable, Deck deck) {
   PrintTable(cardsOnTable);
 }
 
-void CoverMatching(Card *cardsOnTable, Deck deck) {
+void CoverMatching(Card &currentCard, Card *cardsOnTable, Deck deck, bool &Win, bool &Lost, int &matches) {
   //for(unsigned int x{0}; x < 8; x++) {
-    Card currentCard = deck.GetNextItem();
-    cout << "Card on top of deck: " << currentCard.ToString() << endl;
-    int matches{0};
+    unsigned int y{0};
+    int newMatch = matches;
 
-    for(unsigned int y{0}; y < 8; y++){
-      Card currentComparison = cardsOnTable[y];
-      if(currentCard.GetRank() == currentComparison.GetRank()){
-        cardsOnTable[y] = currentCard;
-        cout << "Found a match!" << endl;
+    // Display the current cards before matching and stacking
+    cout << "\nTable: \n";
+    PrintTable(cardsOnTable);
+
+    // shows the card that is being compared to all of the cards on top of the table
+    cout << "\nCard on top of deck: " << currentCard.ToString() << endl;
+    cout << endl;
+    if((matches + 8) == 50) { // Check if the deck is empty by seeing if 42 matches have been made (The deck is 50 cards large, 8 cards are needed to start)
+      cout << endl;
+      PrintTable(cardsOnTable);
+      cout << "\n\nYou Won!\n";
+      Win = true;
+    }
+    else {
+      while((y <= 8 && Lost == false) && Win == false){
+
+        Card currentComparison = cardsOnTable[y];
+
+        if(currentCard.GetRank() == currentComparison.GetRank()){ //compare the card on the top of the deck to the current card
+          cardsOnTable[y] = currentCard; // put the card on top of it's match
+          matches++; // increment the amount of matches made
+
+          cout << "Matches: " << matches << endl << endl; // display the amount of matches
+          cout << currentCard.ToString() << " stacked onto " << currentComparison.ToString() << endl;
+          cout << endl;
+
+          if((matches + 8) == 50) { // Check if the deck is empty by seeing if 42 matches have been made (The deck is 50 cards large, 8 cards are needed to start)
+            cout << endl;
+            PrintTable(cardsOnTable);
+            cout << "\n\nYou Won!\n";
+            Win = true;
+          }
+        }
+        y++; //Increment counter
       }
-      if(matches == 0) {
-        cout << "No matches were found!";
+      if(matches == newMatch) { // Check if there aren't any matching cards
+          cout << "No matches were found! Round Lost.\n";
+          Lost = true;
       }
+
     }
   //}
 }
