458d994765617f7102c9138d748caa4319a4ddda
Smaccus <77114635+l-Xevia-l@users.noreply.github.com>
2022-10-16 15:56:17 -0500

    Update deckDriver.cpp

--- Big Programs/Program1/deckDriver.cpp
+++ Big Programs/Program1/deckDriver.cpp
@@ -29,7 +29,7 @@ void PrintTable(const Card[]);
 
 void PlaceCards(Card[], Deck);
 
-void CoverMatching(Card &currentCard, Card[], Deck, bool &Win, bool &Lost, int &matches);
+void CoverMatching(Card &topOfDeck, Card[], Deck, bool &Win, bool &Lost, int &matches);
 
 int main() {
   srand(time(NULL));
@@ -38,10 +38,10 @@ int main() {
   bool Lost{false};
 
   int attempts{0};
-  Card currentCard;
 
   Deck deck;
-
+  deck.GenerateDeck();
+  
   // Card slots on table
   Card Card1_1, Card1_2, Card1_3, Card1_4, 
        Card2_1, Card2_2, Card2_3, Card2_4;
@@ -50,29 +50,40 @@ int main() {
   Card cardsOnTable[8] = {Card1_1, Card1_2, Card1_3, Card1_4, 
                           Card2_1, Card2_2, Card2_3, Card2_4};
 
+  Card topOfDeck;
+  
+  int mostMatches{0};
+
   while(Win == false){ 
     int matches{0};
-    
+
     // Generate and shuffle the deck
-    deck.GenerateDeck();
-    deck.ResetList();
-    for(unsigned int i{0}; i < (rand() % 50 + 20); i++) {
+    for(unsigned int i{0}; i < (rand() % 100 + 50); i++) {
       deck.Shuffle();
     }
 
-
-    deck.ResetList();
+    // Show the beginning 8 cards
+    cout << "Starting cards:" << endl;
     PlaceCards(cardsOnTable, deck);
-    
+    cout << endl;
+    // Makes sure that the cards in cardsOnTable are not compared to themselves
+    for(unsigned int i{0}; i < 8; i++) {
+      deck.GetNextItem();
+    }
+
     // Plays one round
     while(Lost == false && Win == false) {
       cout << "-------------------------------------------------------";
-      currentCard = deck.GetNextItem();
-      CoverMatching(currentCard, cardsOnTable, deck, Win, Lost, matches);
+      topOfDeck = deck.GetNextItem();
+      CoverMatching(topOfDeck, cardsOnTable, deck, Win, Lost, matches);
+    }
+    if(matches > mostMatches) {
+      mostMatches = matches;
     }
     if(Lost == true){
       Lost = false;
       cout << "\n\n\n\n\n\n\n  ------------Attempts: " << attempts << "------------\n\n\n\n\n\n\n";
+      cout << "\n  ------------highest match spree: " << mostMatches << "------------\n";
       cout << "\n\n  Retrying...\n\n";
     }
     //this_thread::sleep_for(std::chrono::nanoseconds(1));    // for debugging
@@ -119,7 +130,7 @@ void PlaceCards(Card *cardsOnTable, Deck deck) {
   PrintTable(cardsOnTable);
 }
 
-void CoverMatching(Card &currentCard, Card *cardsOnTable, Deck deck, bool &Win, bool &Lost, int &matches) {
+void CoverMatching(Card &topOfDeck, Card *cardsOnTable, Deck deck, bool &Win, bool &Lost, int &matches) {
   //for(unsigned int x{0}; x < 8; x++) {
     unsigned int y{0};
     int newMatch = matches;
@@ -129,7 +140,7 @@ void CoverMatching(Card &currentCard, Card *cardsOnTable, Deck deck, bool &Win, 
     PrintTable(cardsOnTable);
 
     // shows the card that is being compared to all of the cards on top of the table
-    cout << "\nCard on top of deck: " << currentCard.ToString() << endl;
+    cout << "\nCard on top of deck: " << topOfDeck.ToString() << endl;
     cout << endl;
     if((matches + 8) == 50) { // Check if the deck is empty by seeing if 42 matches have been made (The deck is 50 cards large, 8 cards are needed to start)
       cout << endl;
@@ -142,12 +153,13 @@ void CoverMatching(Card &currentCard, Card *cardsOnTable, Deck deck, bool &Win, 
 
         Card currentComparison = cardsOnTable[y];
 
-        if(currentCard.GetRank() == currentComparison.GetRank()){ //compare the card on the top of the deck to the current card
-          cardsOnTable[y] = currentCard; // put the card on top of it's match
+        if(topOfDeck.GetRank() == currentComparison.GetRank()){ //compare the card on the top of the deck to the current card
+          Card cardUnder = cardsOnTable[y];
+          cardsOnTable[y] = topOfDeck; // put the card on top of it's match
           matches++; // increment the amount of matches made
 
           cout << "Matches: " << matches << endl << endl; // display the amount of matches
-          cout << currentCard.ToString() << " stacked onto " << currentComparison.ToString() << endl;
+          cout << topOfDeck.ToString() << " stacked onto " << cardUnder.ToString() << endl;
           cout << endl;
 
           if((matches + 8) == 50) { // Check if the deck is empty by seeing if 42 matches have been made (The deck is 50 cards large, 8 cards are needed to start)
